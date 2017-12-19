78a0d87612163c7db1432fbf8951726522dbe204
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-12-19 15:40:55 -0500

    Fix the weird way the pylib was installed

--- src/python/pyinstall.cmake
+++ src/python/pyinstall.cmake
@@ -1,1 +1,4 @@
-install(CODE "execute_process( COMMAND bash \"${CMAKE_CURRENT_SOURCE_DIR}\"/buildscript.sh \"${CMAKE_CURRENT_SOURCE_DIR}\" ${PYTHON} install )" )
+cmake_minimum_required(VERSION 3.1.3)
+set(Python_ADDITIONAL_VERSIONS 2.7 2.6 2.5)
+find_package(PythonInterp REQUIRED)
+install(CODE "execute_process( COMMAND bash \"${CMAKE_CURRENT_SOURCE_DIR}\"/buildscript.sh \"${CMAKE_CURRENT_SOURCE_DIR}\" ${PYTHON_EXECUTABLE} install )" )
